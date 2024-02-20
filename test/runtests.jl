3b6aa420f9f9bf956f738ffbb7a9a0a96ce74902
abap34 <uchi_ymgc@her0.jp>
2024-02-20 18:38:05 +0900

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -12,34 +12,23 @@ end
 
 
 testcases = [
-    ("\"hello\"", Minia.str) => "hello",
-    ("10", Minia.num) => 10,
-    ("0", Minia.num) => 0,
-    ("1.25", Minia.num) => 1.25,
-    ("-1", Minia.unary) => -1,
-    ("10 * 10", Minia.mul) => 100,
-    ("10 * 20 / 10", Minia.mul) => 20,
-    ("1 + 2", Minia.add) => 3,
-    ("1 + 2 * 3", Minia.add) => 7,
-    ("1.5 * 2 - 10", Minia.add) => -7,
-    ("a = 10", Minia.assign) => 10,
-    ("a + 2", Minia.add) => 12,
-    ("10 < 20", Minia.relational) => true,
-    ("10 > 20", Minia.relational) => false,
-    ("10 == 10", Minia.relational) => true,
-    ("10 != 10", Minia.relational) => false,
-    ("cos(0)", Minia.call) => 1.0,
-    ("i = 0", Minia.assign) => 0,
-    ("println(i)", Minia.call) => nothing,
+    ("\"hello\"", Minia.str) => (isequal("hello")),
+    ("1", Minia.int) => (isequal(1)),
+    ("10", Minia.int) => (isequal(10)),
+    ("10.0", Minia.float) => (isequal(10.0)),
+    ("10 + 20", Minia.add) => (isequal(30)),
+    ("10 * 20 + 10", Minia.expr) => (isequal(210)),
+    ("\n", Minia.newline) => ((_) -> true),
+    ("", Minia.maynewline) => ((_) -> true),
 ]
 
 
 
 
 function check(src::String, rule, expected)
-    ast = PEG.parse_whole(rule, Minia.preprocess(src))
+    ast = Minia._parse(src, rule=rule)
     result = eval(ast)
-    success = result == expected
+    success = expected(result)
     return onfail(@test success) do
         @info "Failed. Expected: $expected, got: $result"
         @info "AST: $ast"
