2f545bc138173c32be108da160de724da7725866
abap34 <uchi_ymgc@her0.jp>
2024-02-25 23:18:16 +0900

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,4 @@
 using Minia
-using PEG
 using Test
 
 onfail(body, _::Test.Pass) = true
@@ -10,35 +9,65 @@ function load(filename::AbstractString)
     return join(readlines(filename), "\n")
 end
 
+function iserror(; expectederror::Type{<:Exception}=Exception)
+    return ((e) -> e isa expectederror)
+end
+
+parseerror() = iserror(expectederror=Base.Meta.ParseError)
 
 testcases = [
     ("\"hello\"", Minia.str) => (isequal("hello")),
     ("1", Minia.int) => (isequal(1)),
     ("10", Minia.int) => (isequal(10)),
     ("10.0", Minia.float) => (isequal(10.0)),
-    ("10 + 20", Minia.add) => (isequal(30)),
-    ("10 * 20 + 10", Minia.expr) => (isequal(210)),
-    ("\n", Minia.newline) => ((_) -> true),
-    ("", Minia.maynewline) => ((_) -> true),
+    (".0", Minia.float) => (iserror()),
+    ("0.", Minia.float) => (iserror()),
+    ("true", Minia.bool) => (isequal(true)),
+    ("false", Minia.bool) => (isequal(false)),
+    ("123.45", Minia.num) => (isequal(123.45)),
+    ("123", Minia.num) => (isequal(123)),
+    ("abc", Minia.ident) => (isequal(UndefVarError(:abc))),
+    ("abc_123", Minia.ident) => (isequal(UndefVarError(:abc_123))),
+    ("123abc", Minia.ident) => (parseerror()),
+    ("if", Minia.ident) => (iserror()),
+    ("true", Minia.ident) => (iserror()),
+    ("1.0", Minia.unary) => (isequal(1.0)),
+    ("-1.0", Minia.unary) => (isequal(-1.0)),
+    ("-1", Minia.unary) => (isequal(-1)),
+    ("1", Minia.unary) => (isequal(1)),
+    ("\"hello\"", Minia.unary) => (isequal("hello")),
+    ("true", Minia.unary) => (isequal(true)),
+    ("false", Minia.unary) => (isequal(false)),
+    ("abc", Minia.unary) => (isequal(UndefVarError(:abc))),
+    ("abc_123", Minia.unary) => (isequal(UndefVarError(:abc_123))),
+    ("123abc", Minia.unary) => (parseerror()),
+    ("if", Minia.unary) => (iserror()),
+    ("true", Minia.unary) => (isequal(true)),
 ]
 
 
-
-
-function check(src::String, rule, expected)
-    ast = Minia._parse(src, rule=rule)
-    result = eval(ast)
-    success = expected(result)
-    return onfail(@test success) do
-        @info "Failed. Expected: $expected, got: $result"
-        @info "AST: $ast"
+function check(src::String, rule::Function, expected::Function)
+    try
+        ast = Minia._parse(src, rule=rule)
+        result = eval(ast)
+        success = expected(result)
+        onfail(@test success) do
+            @info "Failed. Expected: $expected, got: $result"
+            @info "AST: $ast"
+        end
+    catch e
+        result = e
+        success = expected(e)
+        onfail(@test success) do
+            @info "Failed. Expected: $expected, got error: $e"
+        end
     end
+
 end
 
 
 @testset "Minia.jl" begin
     for ((src, rule), expected) in testcases
-        @info "Test for... \n$src"
         check(src, rule, expected)
     end
 end
